a0107062159e6167b03cd60ce4f892aabd595a02
Cory Westerfield <cory.westerfield@gmail.com>
2020-05-08 11:51:07 -0600

    Add nested route for Artists and Songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,4 +6,21 @@ class SongsController < ApplicationController
   def show
     @song = Song.find(params[:id])
   end
+
+  def new
+    @artist_id = params[:artist_id]
+  end
+
+  def create
+    artist = Artist.find(params[:artist_id])
+    song = artist.songs.create!(song_params)
+
+    redirect_to "/songs/#{song.id}"
+  end
+
+  private
+
+  def song_params
+    params.permit(:title, :length, :play_count)
+  end
 end
